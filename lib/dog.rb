46650ecf290dd4f962e8c4e976bfb1a0da4894a3
Scarlett Perry <si.perry15@gmail.com>
2018-07-18 02:32:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
   def name=(dog_name) #sets dog's name
-    @this_dogs_name=dog_name
+    @this_dogs_name=dog_name #set to an instance variable so that it can be accessed by other methods 
   end 
   
   def name #gets dog's name 
-    this_dogs_name
+    @this_dogs_name
   end 
 end 
 
